0149dd08142868182ca801be032dfe4cc39b8066
Rich <r.j.lewindon@hotmail.co.uk>
2020-05-09 08:02:42 +0100

    Added objects to hold the 25 scenarios for the game

---
+++ myjs.js
@@ -0,0 +1,39 @@
+let peasant = {
+	first: "P Task 1",
+	second: "P Task 2",
+	third: "P Task 3",
+	fourth: "P Task 4",
+	fifth: "P Task 5"
+};
+
+let landOwner = {
+	first: "O Task 1",
+	second: "O Task 2",
+	third: "O Task 3",
+	fourth: "O Task 4",
+	fifth: "O Task 5"
+};
+
+let knight = {
+	first: "K Task 1",
+	second: "K Task 2",
+	third: "K Task 3",
+	fourth: "K Task 4",
+	fifth: "K Task 5"
+};
+
+let lord = {
+	first: "L Task 1",
+	second: "L Task 2",
+	third: "L Task 3",
+	fourth: "L Task 4",
+	fifth: "L Task 5"
+};
+
+let king = {
+	first: "Ki Task 1",
+	second: "Ki Task 2",
+	third: "Ki Task 3",
+	fourth: "Ki Task 4",
+	fifth: "Ki Task 5"
+};
