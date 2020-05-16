c4d9062645bfae8f11b30024c769ed7471736bee
Rich <r.j.lewindon@hotmail.co.uk>
2020-05-16 10:24:25 +0100

    Tidyed up and removed unused code

--- myjs.js
+++ myjs.js
@@ -29,6 +29,21 @@ let numberOfQuestionsPerLevel = 3;
 let promoted = false;
 let score = 0;
 
+//Ui elements
+let scenarioUI;
+let answerOneUi;
+let answerTwoUi;
+let answerThreeUi;
+let resultUI;
+let buttonTryAgain;
+let goldUi;
+let levelUi;
+let buttonContinue;
+let gameUi;
+let scorePelement;
+let image;
+let scoreContainer;
+let scoreImage;
 
 
 let taskOne = new Task("You are working the fields for your land-owner when an unknown man approaches you. He has a scar on his face and you feel an evil energy from him. He says if you murder your land-owner then he will forge the papers to give you the land. What do you do?", 
@@ -94,21 +109,6 @@ let taskFifteen = new Task("The Lord's aims are now clear. He has abandoned any 
 
 const arr = [taskOne, taskTwo, taskThree, taskFour, taskFive, taskSix, taskSeven, taskEight, 
 taskNine, taskTen, taskEleven, taskTwelve, taskThirteen, taskFourteen, taskFifteen];
-
-let scenarioUI;
-let answerOneUi;
-let answerTwoUi;
-let answerThreeUi;
-let resultUI;
-let buttonTryAgain;
-let goldUi;
-let levelUi;
-let buttonContinue;
-let gameUi;
-let scorePelement;
-let image;
-let scoreContainer;
-let scoreImage;
 
 function nextTaskOne() {
 	let theOption = arr[status].optionOne;
@@ -445,49 +445,3 @@ function displayTask(myStatus) {
 }
 
 window.onload = init;
-
-
-console.log(taskTwo);
- 
-let peasant = {
-	first: taskOne,
-	second: taskTwo,
-	third: taskThree,
-	fourth: taskFour,
-	fifth: taskFive
-};
-
-console.log(peasant);
-
-let landOwner = {
-	first: "O Task 1",
-	second: "O Task 2",
-	third: "O Task 3",
-	fourth: "O Task 4",
-	fifth: "O Task 5"
-};
-
-let knight = {
-	first: "K Task 1",
-	second: "K Task 2",
-	third: "K Task 3",
-	fourth: "K Task 4",
-	fifth: "K Task 5"
-};
-
-let lord = {
-	first: "L Task 1",
-	second: "L Task 2",
-	third: "L Task 3",
-	fourth: "L Task 4",
-	fifth: "L Task 5"
-};
-
-let king = {
-	first: "Ki Task 1",
-	second: "Ki Task 2",
-	third: "Ki Task 3",
-	fourth: "Ki Task 4",
-	fifth: "Ki Task 5"
-};
-
